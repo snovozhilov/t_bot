4fcc8e2b4155aa948b2222d059e7715816605085
Sergey Novozhilov <58106550+snovozhilov@users.noreply.github.com>
2019-11-30 04:46:53 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,4 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-API_KEY = '999221800:AAFUqfE7eQIoEm8HqlBV6cFTFsnN_GZtw4k'
